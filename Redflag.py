fb2bf2cd53d160c636b5f20424c1e28cafd9c0ad
Sc4r3Cr0w <52085661+UncleJ4ck@users.noreply.github.com>
2023-11-10 21:18:05 +0100

    Update Redflag.py

--- Redflag.py
+++ Redflag.py
@@ -1,27 +1,41 @@
 import sys
 import requests
-from datetime import date, timedelta
+import re
 
-today = date.today()
-one_day_ago = str(today - timedelta(days=1))
-
-WEBSITE_URL = "https://red.flag.domains/posts/" + one_day_ago
-print(WEBSITE_URL)
-
-async def check_for_word(word):
-    response = requests.get(WEBSITE_URL)
+def fetch_and_search(url, domain):
+    response = requests.get(url)
     if response.status_code == 200:
-        if word in response.text:
-            message = f"Warning: A potential domain similar to '{word}' was recently registered. Make sure you are not a victim of phishing attacks. Check the source here: {WEBSITE_URL}"
-            print(message)
-        else:
-            print("No relevant word found.")
+        if re.search(r'\b' + re.escape(domain) + r'\b', response.text):
+            return True
+    return False
+
+def check_domain(domain):
+    urls = [
+        "https://dl.red.flag.domains/red.flag.domains.txt",
+        "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-links-NEW-today.txt",
+        "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-links-ACTIVE.txt",
+        "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-links-ACTIVE-today.txt",
+        "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-links-ACTIVE-TODAY.txt",
+        "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-links-ACTIVE-NOW.txt",
+        "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-domains-NEW-last-hour.txt",
+        "https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-domains-ACTIVE.txt"
+    ]
+
+    found_urls = []
+    for url in urls:
+        if fetch_and_search(url, domain):
+            found_urls.append(url)
+
+    if found_urls:
+        print(f"Domain '{domain}' was found in the following lists:")
+        for url in found_urls:
+            print(url)
     else:
-        print(f"Could not connect to the website {WEBSITE_URL}.")
+        print(f"Domain '{domain}' was not found in any list.")
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: python script.py <word>")
+        print("Usage: python script.py <domain>")
     else:
-        word = sys.argv[1]
-        asyncio.run(check_for_word(word))
+        domain = sys.argv[1]
+        check_domain(domain)
