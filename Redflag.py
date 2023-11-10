8cfc24349fa1cc45f666fc258d29c010e3774ad6
Sc4r3Cr0w <52085661+UncleJ4ck@users.noreply.github.com>
2023-11-10 16:19:44 +0100

    Update Redflag.py

--- Redflag.py
+++ Redflag.py
@@ -1,10 +1,6 @@
 import sys
 import requests
-import telegram
 from datetime import date, timedelta
-import asyncio
-import tracemalloc
-tracemalloc.start()
 
 today = date.today()
 one_day_ago = str(today - timedelta(days=1))
@@ -12,20 +8,14 @@ one_day_ago = str(today - timedelta(days=1))
 WEBSITE_URL = "https://red.flag.domains/posts/" + one_day_ago
 print(WEBSITE_URL)
 
-# Replace <YOUR_BOT_TOKEN> with your Telegram bot token
-bot = telegram.Bot(token='##############################')
-# Replace <CHAT_ID> with the ID of the chat you want to send the message to
-chat_id = '########'
-
 async def check_for_word(word):
     response = requests.get(WEBSITE_URL)
     if response.status_code == 200:
         if word in response.text:
-            message = f"Warning a potential domain similare to urs '{word}' was recently registred, make sure to not be victime of phishing attacks, check the source here>"
-            await bot.send_message(chat_id=chat_id, text=message)
-            print("Message sent to Telegram.")
+            message = f"Warning: A potential domain similar to '{word}' was recently registered. Make sure you are not a victim of phishing attacks. Check the source here: {WEBSITE_URL}"
+            print(message)
         else:
-            print(f"Nothing")
+            print("No relevant word found.")
     else:
         print(f"Could not connect to the website {WEBSITE_URL}.")
 
